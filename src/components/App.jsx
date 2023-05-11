638eb530122501aa2a94269e50cb4d3164c008b2
c0d31t <mokriianastasia@gmail.com>
2023-05-11 19:23:48 -0400

    add App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Container } from './App.styled';
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      {/* <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} /> */}
+    </Container>
   );
 };
+
+export default App;
