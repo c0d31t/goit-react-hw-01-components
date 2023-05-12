038d8179474b05b29c096aadcccae04e5ce89aaa
c0d31t <mokriianastasia@gmail.com>
2023-05-11 20:03:45 -0400

    add Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Container } from './App.styled';
 import Profile from './Profile/Profile';
 import user from './Profile/user.json';
+import Statistics from './Statistics/Statistics';
+import data from './Statistics/data.json';
 
 export const App = () => {
   return (
@@ -12,8 +14,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <FriendList friends={friends} />
       <TransactionHistory items={transactions} /> */}
     </Container>
   );
